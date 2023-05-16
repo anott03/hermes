be0b2a9958c5b1fba840f6feff1aa635767e545a
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-05-15 21:33:38 -0400

    working boilerplate with auth

---
+++ src/app/dash/page.tsx
@@ -0,0 +1,5 @@
+export default function Dash() {
+
+    return <div className="w-full h-full">
+    </div>;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,48 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['"FiraCode Nerd Font"'],
+        serif: ['"FiraCode Nerd Font"'],
+        mono: ['"FiraCode Nerd Font"'],
+      },
+      colors: {
+        red: {
+          950: "#cc241d",
+        },
+        green: {
+          950: "#98971a",
+        },
+        yellow: {
+          950: "#d79921",
+        },
+        blue: {
+          950: "#458588",
+        },
+        purple: {
+          950: "#b16286",
+        },
+        aqua: {
+          950: "#689d6a",
+        },
+      },
+      fontSize: {
+        xs: ".75rem",
+        sm: ".875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
       },
     },
   },
   plugins: [],
-}
+};
