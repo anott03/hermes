f58cf4407da8d6d106baf99e8aa34739e93639bc
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-05-29 14:29:42 -0400

    Icons refactor and some styles things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,5 @@
+import { ArrowRight } from "@/components/Icons";
 import Link from "next/link";
-
-// https://feathericons.dev/?search=chevron-right&iconset=feather&format=strict-tsx
-export function ChevronRight(props: JSX.IntrinsicElements["svg"]) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="20"
-      height="20"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      {...props}
-    >
-      <polyline points="9 18 15 12 9 6" />
-    </svg>
-  );
-}
-
-// https://feathericons.dev/?search=arrow-right&iconset=feather&format=strict-tsx
-export function ArrowRight(props: JSX.IntrinsicElements["svg"]) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      width="20"
-      height="20"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      {...props}
-    >
-      <line x1="5" x2="19" y1="12" y2="12" />
-      <polyline points="12 5 19 12 12 19" />
-    </svg>
-  );
-}
 
 export default function Home() {
   return (
