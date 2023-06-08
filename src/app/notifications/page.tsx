f71d9e116a86aa7ed300685c19846c91cc0d7b66
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-06-08 16:00:57 -0400

    accepting and declining friend requests

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,36 +1,89 @@
 import { db } from "@/db/drizzle";
-import { notifications, type Notification } from "@/db/schema";
+import { notifications, type Notification, User } from "@/db/schema";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
-
-function Notification({ notification }: { notification: Notification }) {
-    return (
-        <div className="p-2">
-            {notification.message}
-            <button className="p-1 text-sm hover:bg-gray-100">Accept</button>
-            <button className="p-1 text-sm hover:bg-gray-100">Decline</button>
-        </div>
-    )
-}
+import { revalidatePath } from "next/cache";
+import { RefreshCw } from "@/components/Icons";
 
 export default async function Notifications() {
     const { userId } = auth();
     if (!userId) {
         return NextResponse.redirect("http://localhost:3000/sign-in");
     }
-    const user = await clerkClient.users.getUser(userId);
+    const user = await clerkClient.users.getUser(userId) as User;
     const userEmail = user.emailAddresses[0].emailAddress;
     const userNotifications = await db.query.notifications.findMany({
         where: eq(notifications.recipientEmail, userEmail)
     });
 
+    const rejectNotification = async (i: number) => {
+        "use server";
+        const n = userNotifications[i];
+        db.delete(notifications)
+            .where(eq(notifications.id, n.id))
+            .execute();
+        revalidatePath("/notifications");
+    }
+
+    const acceptNotification = async (i: number) => {
+        "use server";
+        const _userId = auth().userId;
+        if (!_userId) return;
+        const u = await clerkClient.users.getUser(_userId) as User;
+
+        const n = userNotifications[i];
+        const friend = await clerkClient.users.getUser(n.senderId) as User;
+        const friendFriends = friend.privateMetadata.friends;
+        await clerkClient.users.updateUser(friend.id, {
+            privateMetadata: {
+                friends: [...friendFriends, user.id],
+            }
+        });
+        await clerkClient.users.updateUser(user.id, {
+            privateMetadata: {
+                friends: [...u.privateMetadata.friends, friend.id],
+            }
+        });
+        db.delete(notifications)
+            .where(eq(notifications.id, n.id))
+            .execute();
+        revalidatePath("/notifications");
+    }
+
     return (
         <div className="w-full h-full flex flex-col justify-center items-center bg-[#fbf9fc]">
             <div className="bg-white min-w-[500px] max-w-[600px] min-h-[500px] max-h-[600px] w-4/12 h-[75%] rounded rounded-lg shadow-xl drop-shadow-xl shadow-purple-500 flex flex-col justify-center items-start  p-3">
-                <p className="text-2xl">Notifications</p>
+                <div className="flex flex-row items-center justify-between w-full">
+                    <p className="text-2xl">Notifications</p>
+                    <form action={async () => {
+                        "use server";
+                        revalidatePath("/notifications");
+                    }}>
+                        <button><RefreshCw /></button>
+                    </form>
+                </div>
                 <div className="w-full h-full flex-1">
-                    {userNotifications.map((notification, i) => <Notification key={i} notification={notification} />)}
+                    <div className="border-b border-violet-300"></div>
+                    {userNotifications.length > 0 ? userNotifications.map((notification, i) =>
+                        <div key={i} className="p-2 flex flex-col border-b border-violet-300">
+                            <p className="text-sm">{notification.message}</p>
+                            <div className="w-full justify-end flex flex-row">
+                                <form action={async () => {
+                                    "use server";
+                                    acceptNotification(i)
+                                }}><button className="p-2 text-sm hover:bg-violet-100">Decline</button></form>
+                                <form action={async () => {
+                                    "use server";
+                                    rejectNotification(i)
+                                }}><button className="p-2 text-sm hover:bg-violet-100">Accept</button></form>
+                            </div>
+                        </div>
+                    ) :
+                        <div className="w-full h-full flex-1 flex flex-col justify-center items-center">
+                            <p className="text-sm">Your inbox is empty!</p>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,6 @@
 import { UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Bell, UserPlus } from "./Icons";
 import Link from "next/link";
-import { LegacyRef, forwardRef, useEffect, useRef } from "react";
-
-// const Notifications = forwardRef(function Notifications({ closeModal }: { closeModal: () => void }, ref: LegacyRef<HTMLDialogElement>) {
-//     useEffect(() => {
-//         fetch("/api/notifications")
-//             .then(res => console.log(res))
-//             .then(json => console.log(json));
-//     });
-//
-//     return (
-//         <dialog ref={ref}
-//             className="hidden w-screen h-screen bg-white sm:min-w-[500px] sm:max-w-[600px] sm:min-h-[500px] sm:max-h-[600px] sm:w-4/12 sm:h-[75%] rounded rounded-lg open:flex flex-col justify-center items-start backdrop:bg-black backdrop:opacity-50"
-//         >
-//             <div className="w-full flex flex-row justify-between">
-//                 Notifications
-//                 <button onClick={closeModal}>X</button>
-//             </div>
-//             <div className="w-full h-full flex-1">
-//             </div>
-//         </dialog>
-//     );
-// });
 
 export default function Nav() {
     return (<>
